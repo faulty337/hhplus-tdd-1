a8da386620146ac054af8a49a7c1375ea8bc158a
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-06-16 19:26:42 +0900

    Fix @Repository - Modify @Repository

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
@@ -3,7 +3,6 @@ package io.hhplus.tdd.point.repository;
 import io.hhplus.tdd.point.UserPoint;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserPointRepository {
     public UserPoint findById(long id);
 }
--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepositoryImpl.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepositoryImpl.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd.point.repository;
 
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.UserPoint;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserPointRepositoryImpl implements UserPointRepository{
 
     private final UserPointTable userPointTable;
