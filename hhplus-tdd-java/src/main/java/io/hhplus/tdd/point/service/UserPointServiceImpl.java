4c8c7ff53bd138e43a49df7db48cf1d861218501
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-06-17 21:11:46 +0900

    Add Exception logic - Add CustomException - Add ErrorCode enum

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/service/UserPointServiceImpl.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/service/UserPointServiceImpl.java
@@ -1,6 +1,8 @@
 package io.hhplus.tdd.point.service;
 
 
+import io.hhplus.tdd.CustomException;
+import io.hhplus.tdd.ErrorCode;
 import io.hhplus.tdd.point.UserPoint;
 import io.hhplus.tdd.point.dto.UserPointResponse;
 import io.hhplus.tdd.point.repository.UserPointRepository;
@@ -19,7 +21,9 @@ public class UserPointServiceImpl implements UserPointService{
     @Override
     public UserPointResponse getUserPoint(long userId) {
         //UserPointTable 내부 selectById()함수에서 getOrDefault()를 사용하기에 절대 없는 유저에 대한 상황이 나올 수 없음.
-        UserPoint userPoint = userPointRepository.findById(userId).orElseThrow(RuntimeException::new);
+        UserPoint userPoint = userPointRepository.findById(userId).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+        );
         return new UserPointResponse(userPoint.id(), userPoint.point());
     }
 }
