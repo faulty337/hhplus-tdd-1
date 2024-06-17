f7c1283cb59c70bbc714e4927cb9a38f376c1df4
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-06-17 23:03:23 +0900

    Add getPointHistoryServiceTest - Add success test

--- hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,8 +1,13 @@
 package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.CustomException;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.dto.PointHistoryResponse;
 import io.hhplus.tdd.point.dto.UserPointResponse;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import io.hhplus.tdd.point.repository.PointHistoryRepositoryImpl;
 import io.hhplus.tdd.point.repository.UserPointRepositoryImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +17,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,14 +28,25 @@ class PointServiceTest {
     @Mock
     private UserPointRepositoryImpl userPointRepository;
 
+    @Mock
+    private PointHistoryRepositoryImpl pointHistoryRepository;
+
     @InjectMocks
     private PointServiceImpl userPointService;
+
     private long userId;
     private long point;
+    private final List<PointHistory> pointHistoryList = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
         userId = 1L;
-        point = 1L;
+        point = 100L;
+
+        for(int i = 0; i < 5; i++){
+            pointHistoryList.add(new PointHistory(i, userId, 20, TransactionType.CHARGE, System.currentTimeMillis()));
+        }
+
         MockitoAnnotations.openMocks(this);
     }
 
@@ -37,6 +55,7 @@ class PointServiceTest {
     void getUserPointSuccessPointTest() {
 
         UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+
         when(userPointRepository.findById(userId)).thenReturn(Optional.of(userPoint));
 
         UserPointResponse response = userPointService.getUserPoint(userId);
@@ -49,12 +68,29 @@ class PointServiceTest {
     @Test
     @DisplayName("포인트 조회 - Not Found UserId Exception 테스트")
     void getUserPointNotFoundUserIdTest(){
-        UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
 
         when(userPointRepository.findById(userId)).thenReturn(Optional.empty());
+
         assertThrows(CustomException.class, () ->{
             userPointService.getUserPoint(userId);
         });
     }
+
+    @Test
+    @DisplayName("포인트 로그 조회 - 서비스 로직 작동 테스트")
+    void getPointHistorySuccessPointTest() {
+
+        when(pointHistoryRepository.findAllByUserId(userId)).thenReturn(pointHistoryList);
+
+        List<PointHistoryResponse> response = userPointService.getPointHistory(userId);
+        assertNotNull(response);
+        assertEquals(pointHistoryList.size(), response.size());
+        assertEquals(pointHistoryList.get(0).userId(), response.get(0).userId());
+        verify(pointHistoryRepository, times(1)).findAllByUserId(userId);
+
+
+    }
+
+
 
 }
