6fceae0feffad887889e1c3a447a8d75ea67e6fa
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-06-17 22:53:29 +0900

    Add E2E Test - Add success exception test - Add parameter exception test

--- hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
+++ hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
@@ -1,21 +1,21 @@
 package io.hhplus.tdd.point.controller;
 
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
 import io.hhplus.tdd.point.repository.UserPointRepository;
-import io.hhplus.tdd.point.repository.UserPointRepositoryImpl;
-import io.hhplus.tdd.point.service.UserPointService;
-import io.hhplus.tdd.point.service.UserPointServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -33,18 +33,28 @@ class PointControllerTest {
     @MockBean
     private UserPointRepository userPointRepository;
 
+    @MockBean
+    private PointHistoryRepository pointHistoryRepository;
+
 
     private long userId;
 
     private long point;
+
+    private final List<PointHistory> pointHistoryList = new ArrayList<>();
 
     @BeforeEach
     public void setUP(){
         userId = 1L;
         point = 100L;
 
+        for(int i = 0; i < 5; i++){
+            pointHistoryList.add(new PointHistory(i, userId, 20, TransactionType.CHARGE, System.currentTimeMillis()));
+        }
+
         UserPoint userPoint = new UserPoint(userId, point);
         when(userPointRepository.findById(userId)).thenReturn(Optional.of(userPoint));
+        when(pointHistoryRepository.findAllByUserId(userId)).thenReturn(pointHistoryList);
 
     }
 
@@ -77,5 +87,25 @@ class PointControllerTest {
         mockMvc.perform(get("/point/{id}", nonUserId))
                 .andExpect(status().is5xxServerError());
     }
+
+    @Test
+    @DisplayName("포인트 로그 조회 - 작동 테스트")
+    void pointHistoryAPISuccessTest() throws Exception {
+
+        mockMvc.perform(get("/point/{id}/histories", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(pointHistoryList.size()))
+                .andExpect(jsonPath("$[0].userId").value(userId))
+                .andExpect(jsonPath("$[0].amount").value(20));
+    }
+
+    @Test
+    @DisplayName("포인트 로그 조회 - 정수 이외 테스트")
+    void pointHistoryAPIParameterExceptionTest() throws Exception {
+        String userId = "테스트";
+        mockMvc.perform(get("/point/{id}/histories", userId))
+                .andExpect(status().is4xxClientError());
+    }
+
 
 }
